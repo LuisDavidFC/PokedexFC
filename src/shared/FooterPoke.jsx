1b9b5690786b025e3e896ac2f4bcc97e45bf55c9
Luis David <luischinchilla12364@gmail.com>
2022-09-05 23:57:23 -0400

    pokeapi

---
+++ src/shared/FooterPoke.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../shared/style/footerPoke.css'
+
+const FooterPoke = () => {
+  return (
+    <footer className='red-rectangle'>
+      <div className='black-rectangle'></div>
+      <div className='circle-ext'>
+        <div className="circle-int"></div>
+      </div>
+    </footer>
+  )
+}
+
+export default FooterPoke
